ab95a79fd0ede53d58b42f9c3b51263ff75a739d
iman nasiri <iman.nassiri@gmail.com>
2024-12-03 23:59:11 +0100

    debuging deploy

--- terraform/main.tf
+++ terraform/main.tf
@@ -50,6 +50,27 @@ resource "aws_iam_role_policy_attachment" "ecr_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
+resource "aws_iam_policy" "ssm_policy" {
+  name        = "ssm-parameter-access-policy"
+  description = "Policy to allow access to SSM Parameter Store"
+  policy      = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect    = "Allow",
+        Action    = "ssm:GetParameter",
+        Resource  = "arn:aws:ssm:eu-central-1:552912569893:parameter/tenant-test/*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_policy_attachment" {
+  role       = aws_iam_role.ec2_ecr_role.name
+  policy_arn = aws_iam_policy.ssm_policy.arn
+}
+
+
 resource "aws_iam_instance_profile" "ec2_profile" {
   name = "ec2-ecr-profile"
   role = aws_iam_role.ec2_ecr_role.name
